d163e52a7863e7242a147e947115efc8a312e626
diego <dieguito.reyes.14xd@gmail.com>
2026-02-16 20:46:46 -0600

    ya esta todo

--- database/migrations/2026_02_06_195427_create_powers_table.php
+++ database/migrations/2026_02_06_195427_create_powers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up(): void
     {
 
-        
+        //
         Schema::create('powers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
